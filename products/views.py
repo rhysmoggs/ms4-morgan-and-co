a9d17a71ff58fbc608a9ffeb6771308edef28d39
rhysmoggs <rhysmoggs@hotmail.com>
2022-11-03 13:57:12 +0000

    Dev: added sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category, Room
 
 # Create your views here.
@@ -12,18 +13,36 @@ def all_products(request):
     query = None
     categories = None
     rooms = None
-
-    if 'category' in request.GET:
-        categories = request.GET['category'].split(',')
-        products = products.filter(category__name__in=categories)
-        categories = Category.objects.filter(name__in=categories)
-
-    if 'room' in request.GET:
-        rooms = request.GET['room'].split(',')
-        products = products.filter(room__name__in=rooms)
-        rooms = Room.objects.filter(name__in=rooms)
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if sortkey == 'room':
+                sortkey = 'room__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'room' in request.GET:
+            rooms = request.GET['room'].split(',')
+            products = products.filter(room__name__in=rooms)
+            rooms = Room.objects.filter(name__in=rooms)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -33,11 +52,14 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_rooms': rooms,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
