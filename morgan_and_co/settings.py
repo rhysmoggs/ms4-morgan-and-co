e061e97d157ad8a80775c90ea41707ceec592653
rhysmoggs <rhysmoggs@hotmail.com>
2022-11-03 17:06:20 +0000

    Dev: added bag context and delivery logic

--- morgan_and_co/settings.py
+++ morgan_and_co/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth, do not delete
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -162,3 +163,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 100
+STANDARD_DELIVERY_PERCENTAGE = 10
