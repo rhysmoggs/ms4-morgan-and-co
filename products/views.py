8f6f7bb1466bb6bfbd7e79bfeceac2add4f4633a
rhysmoggs <rhysmoggs@hotmail.com>
2022-11-03 11:26:03 +0000

    Dev: updated main navbar links, added category and room filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Room
 
 # Create your views here.
 
@@ -10,6 +10,18 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+    rooms = None
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+        products = products.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
+
+    if 'room' in request.GET:
+        rooms = request.GET['room'].split(',')
+        products = products.filter(room__name__in=rooms)
+        rooms = Room.objects.filter(name__in=rooms)
 
     if request.GET:
         if 'q' in request.GET:
@@ -17,13 +29,15 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_rooms': rooms,
     }
 
     return render(request, 'products/products.html', context)
