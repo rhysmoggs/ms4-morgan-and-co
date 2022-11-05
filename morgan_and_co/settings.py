6b436ec30ba5b218563fb2f12a15ca022b83f54b
rhysmoggs <rhysmoggs@hotmail.com>
2022-11-05 09:20:55 +0000

    Dev: added checkout views and templates

--- morgan_and_co/settings.py
+++ morgan_and_co/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'morgan_and_co.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,8 +82,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth, do not delete
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
