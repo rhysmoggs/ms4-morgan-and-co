3c397546d5dcf2f77a2a79ad57cf2100c6ba7a62
rhysmoggs <rhysmoggs@hotmail.com>
2022-11-12 14:22:40 +0000

    Dep: set debug

--- morgan_and_co/settings.py
+++ morgan_and_co/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['morgan-and-co.herokuapp.com', 'localhost']
 
