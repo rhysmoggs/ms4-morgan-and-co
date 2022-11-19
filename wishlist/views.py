1c3501d40b5adaeba6dce4eddf55e1fba486b486
rhysmoggs <rhysmoggs@hotmail.com>
2022-11-19 11:46:31 +0000

    Dev: enhanced get_wishlist view, template and added profile link

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Wishlist
+from profiles.models import UserProfile
 
 
+@login_required
 def get_wishlist(request):
     """ A view to return the wishlist page """
 
-    return render(request, 'wishlist/wishlist.html')
+    user = get_object_or_404(UserProfile, user=request.user)
+    print(user)
+
+    wishlist = None
+    print(wishlist)
+
+    # try to get the user wishlist, if it exists
+    try:
+        get_user_wishlist = Wishlist.objects.get(user=user)
+        print(get_user_wishlist)
+        wishlist = get_user_wishlist.products.all()
+        print(wishlist)
+    except Wishlist.DoesNotExist:
+        pass
+
+    template = 'wishlist/wishlist.html'
+    context = {
+        'wishlist': wishlist,
+    }
+
+    return render(request, template, context)
