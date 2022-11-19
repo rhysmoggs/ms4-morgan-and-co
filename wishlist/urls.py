afe9b0825dfb445a8ef8fbc9c4b63f95a423bab2
rhysmoggs <rhysmoggs@hotmail.com>
2022-11-19 12:21:37 +0000

    Dev: added ability to add products to wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_wishlist, name='wishlist')
+    path('', views.get_wishlist, name='wishlist'),
+    path('add_to_wishlist/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from .models import Wishlist
+from products.models import Product
 from profiles.models import UserProfile
 
 
@@ -30,3 +32,36 @@ def get_wishlist(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """ A view to add a product to user wishlist """
+
+    # get current product
+    product = get_object_or_404(Product, pk=product_id)
+    print(product)
+
+    # get current user
+    user = get_object_or_404(UserProfile, user=request.user)
+    print(user)
+
+    # get user wishlist otherwise create wishlist
+    wishlist, created = Wishlist.objects.get_or_create(user=user)
+    print(wishlist)
+    print(created)
+
+    # check if product exists in user wishlist
+    check_duplicate = bool(
+        Wishlist.objects.filter(user=user, products=product))
+    print(check_duplicate)
+
+    # if product exists in user wishlist, inform user, else, add
+    #  to user wishlist
+    if check_duplicate:
+        messages.info(request, f'{product.name} already in your wishlist.')
+    else:
+        wishlist.products.add(product)
+        messages.success(request, f'{product.name} added to your wishlist.')
+
+    return redirect(reverse('product_detail', args=[product.id]))
